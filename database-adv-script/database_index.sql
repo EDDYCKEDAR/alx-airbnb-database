c7c17364ae478353c0bcc427a1e362c100f12a8d
Eddy Ckedar <eddykedar@gmail.com>
2025-07-06 21:21:07 +0200

    Create database_index.sql

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,12 @@
+-- Indexes for User table
+CREATE INDEX idx_user_email ON User(email);
+CREATE INDEX idx_user_age ON User(age);
+
+-- Indexes for Booking table
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+CREATE INDEX idx_booking_property_id ON Booking(property_id);
+CREATE INDEX idx_booking_created_at ON Booking(created_at);
+
+-- Indexes for Property table
+CREATE INDEX idx_property_location ON Property(location);
+CREATE INDEX idx_property_price ON Property(price);
